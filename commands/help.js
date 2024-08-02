db996e75f5b7f50b9884594e0ec555fc8458f86e
NB <nasrobegg@gmail.com>
2024-08-02 10:22:10 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -12,10 +12,10 @@ module.exports = {
             .addFields(
                 { name: 'Prerequisites', value: 'Ensure you have the necessary roles and permissions. Use the designated channels for commands.' },
                 { name: 'Commands Overview', value: '1. **`v <user_id> [age] [roles]`**\n2. **`top [day|week|month|total]`**\n3. **`myverif [day|week|month|total]`**\n4. **`whoverif <user_id>`**' },
-                { name: '`v` Command', value: '**Usage**: `v <user_id> [age] [roles]`\n**Example**: `verify 123456789012345678 20 english scholar`\nUse in channel ID: `800640245992652840`' },
-                { name: '`vtop` Command', value: '**Usage**: `top [day|week|month|total]`\n**Example**: `top day`\nUse in channel ID: `800545663125422100`' },
-                { name: '`vmyverif` Command', value: '**Usage**: `myverif [day|week|month|total]`\n**Example**: `myverif week`\nUse in channel ID: `800545663125422100`' },
-                { name: '`vwhoverif` Command', value: '**Usage**: `whoverif <user_id>`\n**Example**: `whoverif 123456789012345678`\nUse in channel ID: `800545663125422100`' },
+                { name: '`v` Command', value: '**Usage**: `v <user_id> [age] [roles]`\n**Example**: `verify 123456789012345678 20 english scholar`\nUse in channel : `<#800640245992652840>`' },
+                { name: '`vtop` Command', value: '**Usage**: `top [day|week|month|total]`\n**Example**: `top day`\nUse in channel : `<#800545663125422100>`' },
+                { name: '`vmyverif` Command', value: '**Usage**: `myverif [day|week|month|total]`\n**Example**: `myverif week`\nUse in channel : `<#800545663125422100>`' },
+                { name: '`vwhoverif` Command', value: '**Usage**: `whoverif <user_id>`\n**Example**: `whoverif 123456789012345678`\nUse in channel : `<#800545663125422100>`' },
                 { name: 'Notes', value: 'Always use commands in the designated channels. Ensure you have the required roles. The bot will confirm successful operations or provide error messages if something goes wrong.' }
             )
             .setTimestamp();
