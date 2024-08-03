5fc3a26e069588074d4a09fe004e9d70ab54817a
NB <nasrobegg@gmail.com>
2024-08-03 19:20:20 +0100

    Update reset.js

--- commands/reset.js
+++ commands/reset.js
@@ -1,26 +1,34 @@
 const Verification = require('../models/Verification');
+const config = require('../config.json');
 
 module.exports = {
     name: 'reset',
     async execute(message, args, client) {
-        // Check if the user has the specific ID allowed to use this command
-        const allowedUserId = '1252982138656129148';
-        if (message.author.id !== allowedUserId) {
+        // Check if the user is authorized to use this command
+        if (message.author.id !== '1252982138656129148') {
             return message.reply('You do not have permission to use this command.');
         }
 
         try {
-            // Find all verifications and reset their counts
-            const verifications = await Verification.find({});
-            for (const verification of verifications) {
-                verification.counts = { day: 0, week: 0, month: 0, total: 0 };
-                await verification.save();
+            // Find all moderators with the allowed roles
+            const guild = client.guilds.cache.get(config.guildId);
+            const moderators = guild.members.cache.filter(member => 
+                member.roles.cache.some(role => config.allowedRoles.includes(role.id))
+            );
+
+            // Reset verification counts for all moderators
+            for (const moderator of moderators.values()) {
+                await Verification.updateOne(
+                    { moderatorId: moderator.id },
+                    { $set: { 'counts.day': 0, 'counts.week': 0, 'counts.month': 0, 'counts.total': 0 } },
+                    { upsert: true }
+                );
             }
 
-            message.reply('Successfully reset all verification counts.');
-        } catch (err) {
-            console.error(err);
-            message.reply('There was an error resetting the verification counts.');
+            message.reply('All verification counts have been reset.');
+        } catch (error) {
+            console.error('Error executing reset command:', error);
+            message.reply('There was an error executing the reset command.');
         }
     }
 };
