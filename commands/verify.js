b80eda72556e32ed7c242a64231b593e6b3e8a15
NB <nasrobegg@gmail.com>
2024-08-06 03:08:55 +0100

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -1,43 +1,43 @@
 const { EmbedBuilder } = require('discord.js');
-const Verification = require('../models/Verification');
-const config = require('../config.json');
 const moment = require('moment-timezone');
+const Verification = require('../models/Verification');
 const stringSimilarity = require('string-similarity');
 
 module.exports = {
     name: 'verify',
-    async execute(message, args, client, loadingMessage) {
-        if (!message.member.roles.cache.some(role => config.allowedRoles.includes(role.id))) {
-            await loadingMessage.delete();
-            return message.reply('You do not have permission to use this command.');
+    async execute(message, args) {
+        const loadingMessage = await message.reply('<a:loadingnb:1270196992747896893> Loading...');
+
+        // Check if the user has one of the allowed roles
+        if (!message.member.roles.cache.some(role => ['812318686936825867', '952275776303149176'].includes(role.id))) {
+            return loadingMessage.edit('You do not have permission to use this command.');
         }
 
         const userId = args.shift();
         const user = await message.guild.members.fetch(userId).catch(() => null);
 
         if (!user) {
-            await loadingMessage.delete();
-            return message.reply('User not found.');
+            return loadingMessage.edit('User not found.');
         }
 
-        if (!user.roles.cache.has(config.nonVerifiedRoleId)) {
-            await loadingMessage.delete();
-            return message.reply('This user is already verified.');
+        // Check if the user has the "non-verified" role
+        if (!user.roles.cache.has('862862160156426300')) {
+            return loadingMessage.edit('This user is already verified.');
         }
 
         const age = parseInt(args.find(arg => !isNaN(arg)));
         let ageRole;
         if (age >= 15 && age <= 17) {
-            ageRole = config.roles["15 - 17 YO"];
+            ageRole = '801160166712148020';
         } else if (age >= 18 && age <= 24) {
-            ageRole = config.roles["18 - 24 YO"];
+            ageRole = '952260704679886859';
         } else if (age >= 25 && age <= 30) {
-            ageRole = config.roles["25 - 30 YO"];
+            ageRole = '952260851207925810';
         }
 
         const otherRoles = args.filter(arg => isNaN(arg)).map(role => {
-            const matchedRole = getClosestRoleName(role.trim().toLowerCase(), config.roles);
-            return matchedRole ? config.roles[matchedRole] : null;
+            const matchedRole = getClosestRole(role.trim().toLowerCase(), message.guild.roles.cache);
+            return matchedRole ? matchedRole.id : null;
         }).filter(Boolean);
 
         if (ageRole) {
@@ -45,10 +45,10 @@ module.exports = {
         }
 
         // Always add the "Giveaways" and "Events" roles
-        otherRoles.push(config.roles.Giveaways, config.roles.Events);
+        otherRoles.push('809166576989372466', '801187769179308062');
 
         try {
-            await user.roles.remove(config.nonVerifiedRoleId);
+            await user.roles.remove('862862160156426300');
 
             let assignedRolesMessage = 'No roles assigned';
             if (otherRoles.length) {
@@ -58,40 +58,29 @@ module.exports = {
 
             // Update verification counts in MongoDB
             const moderatorId = message.author.id;
-            let verification = await Verification.findOne({ userId });
+            const verificationDate = new Date();
 
-            if (!verification) {
-                verification = new Verification({
-                    userId,
-                    moderatorId,
-                    verificationDate: new Date(),
-                    assignedRoles: assignedRolesMessage,
-                    counts: { day: 1, week: 1, month: 1, total: 1 }
-                });
-            } else {
-                verification.moderatorId = moderatorId; // Update the moderatorId if the userId already exists
-                verification.verificationDate = new Date();
-                verification.assignedRoles = assignedRolesMessage;
-                verification.counts.day++;
-                verification.counts.week++;
-                verification.counts.month++;
-                verification.counts.total++;
-            }
-            
-            await verification.save();
+            const newVerification = new Verification({
+                userId: user.id,
+                moderatorId,
+                verificationDate,
+                assignedRoles: assignedRolesMessage,
+                counts: { day: 1, week: 1, month: 1, total: 1 }
+            });
 
-            const verificationDate = moment().tz('Africa/Algiers').format('YYYY-MM-DD HH:mm:ss'); // GMT+1
+            await newVerification.save();
+
             const joinDate = moment(user.joinedAt).tz('Africa/Algiers').format('YYYY-MM-DD HH:mm:ss'); // GMT+1
             const accountCreationDate = moment(user.user.createdAt).tz('Africa/Algiers').format('YYYY-MM-DD HH:mm:ss'); // GMT+1
 
-            const verificationEmbed = new EmbedBuilder()
+            const embed = new EmbedBuilder()
                 .setTitle('User Verified')
                 .setColor('#00FF00')
                 .setThumbnail(user.user.displayAvatarURL({ dynamic: true }))
                 .addFields(
                     { name: 'Verified User', value: `${user.user.tag} (<@${user.id}>)` },
                     { name: 'Moderator', value: `${message.author.tag} (<@${message.author.id}>)` },
-                    { name: 'Verification Date', value: verificationDate },
+                    { name: 'Verification Date', value: moment(verificationDate).tz('Africa/Algiers').format('YYYY-MM-DD HH:mm:ss') },
                     { name: 'Join Date', value: joinDate },
                     { name: 'Account Creation Date', value: accountCreationDate },
                     { name: 'Assigned Roles', value: assignedRolesMessage }
@@ -99,33 +88,29 @@ module.exports = {
                 .setFooter({ text: `Verified by ${message.author.tag}`, iconURL: message.author.displayAvatarURL({ dynamic: true }) })
                 .setTimestamp();
 
-            const logChannel = client.channels.cache.get(config.logChannelId);
-            const logMessage = await logChannel.send({ embeds: [verificationEmbed] });
+            const logChannel = client.channels.cache.get('1254231233630834788');
+            const logMessage = await logChannel.send({ embeds: [embed] });
 
-            const replyEmbed = new EmbedBuilder()
+            const responseEmbed = new EmbedBuilder()
                 .setTitle('User Verified')
                 .setColor('#00FF00')
                 .setDescription(`Successfully verified <@${user.id}>. Assigned roles: ${assignedRolesMessage}`)
-                .addFields(
-                    { name: 'View Log Message', value: `[Click Here](${logMessage.url})` }
-                )
+                .addFields({ name: 'View Log Message', value: `[Click Here](${logMessage.url})` })
                 .setTimestamp();
 
-            await loadingMessage.delete();
-            message.reply({ embeds: [replyEmbed] });
+            await loadingMessage.edit({ content: '', embeds: [responseEmbed] });
         } catch (err) {
             console.error(err);
-            await loadingMessage.delete();
-            message.reply('There was an error processing the verification.');
+            loadingMessage.edit('There was an error processing the verification.');
         }
     }
 };
 
-function getClosestRoleName(input, roles) {
-    const roleNames = Object.keys(roles);
-    const matches = stringSimilarity.findBestMatch(input, roleNames);
+function getClosestRole(roleName, allRoles) {
+    const roleNames = allRoles.map(role => role.name);
+    const matches = stringSimilarity.findBestMatch(roleName, roleNames);
     if (matches.bestMatch.rating >= 0.6) {
-        return matches.bestMatch.target;
+        return allRoles.find(role => role.name === matches.bestMatch.target);
     }
     return null;
 }
